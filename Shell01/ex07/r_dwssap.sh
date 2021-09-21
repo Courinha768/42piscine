27065dae32d06671b161606e80edc237323e38ab
Courinha768 <90760852+Courinha768@users.noreply.github.com>
2021-09-21 20:43:51 +0100

    Add files via upload

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,2 +1,1 @@
-cat /etc/passwd | grep -v "^#" | sed '1!n;d' | sed -e 's/:.*//' | rev | sort -r | sed -n "$FT_LINE1 $FT_LINE2 p" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/, /g' | sed 's/$/./' | tr -d '\n'
-
+cat /etc/passwd | sed 's/:[^~]*//g' | sed "/#/d" | sed -n 'n;p'  | rev | sort -r |	sed -n "$FT_LINE1,$FT_LINE2 p" | tr '\n' ',' | sed 's:,:, :g' | sed 's:, $:.:' | tr -d '\n'
